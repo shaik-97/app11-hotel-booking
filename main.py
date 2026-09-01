1770510b99c077aa0186e84a41af9475dc9e0bf0
shaik97 <saleemshaik4297@gmail.com>
2026-09-02 02:32:11 +0530

    added payment classes and cards data

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 import logging
 import pandas as pd
 
-
 logging.basicConfig(
     filename="app11.log",
     filemode="w",
@@ -24,11 +23,12 @@ class Customer(User):
     #     self.loyalty_points += points
     #     log.debug("Added %s loyalty points to user %s", points, self.name)
 
-df = pd.read_csv("hotels.csv").squeeze()
 class Hotel:
     def __init__(self, hotel_id=None,is_availability_24x7=False):
         self.hotel_id = hotel_id
-        self.name = df[df["id"] == hotel_id]["name"]
+        self.name = df.loc[df["id"] == self.hotel_id, "name"].iloc[0] if not df[df["id"] == self.hotel_id].empty else None
+        self.hotel_city = df.loc[df["id"] == self.hotel_id, "city"].values[0] if not df[df["id"] == self.hotel_id].empty else None
+        self.price = df.loc[df["id"] == self.hotel_id, "pernightprice"].values[0] if not df[df["id"] == self.hotel_id].empty else None
         self.is_availability_24x7 = is_availability_24x7
 
     def get_all_hotels(self):
@@ -49,32 +49,22 @@ class Hotel:
         df = pd.read_csv("hotels.csv")
         df.loc[df["id"] == self.hotel_id, "availability"] = "no"
         df.to_csv("hotels.csv", index=False) # False bcz we don't want index column to be added in csv file 
-        print(f'updated list of hotels :- {df}')
+        print(f'updated list of hotels :-\n {df.to_string(index=False)}')
         return
 
     def book_hotel(self,):
         log.debug("Attempting to book hotel id=%s", self.hotel_id)
         if self.is_hotel_id_available(self.hotel_id):
-            hotel_details = self.fetch_hotel_details(self.hotel_id)
-            f"""
-            Hotel booked successfully! -> details : {hotel_details}
-            """
+            log.debug(f"Hotel booked successfully! -> details : \n hotel_id-->{self.hotel_id} \n hotel_name-->{self.name} \n hotel_city-->{self.hotel_city} \n hotel_price-->{self.price}")
             self.updated_hotel_list()
             log.info("Hotel id=%s booked successfully", self.hotel_id)
             return True
         log.warning("Hotel id=%s booking unsuccessful or unavailable", self.hotel_id)
         print("Hotel booking unsuccessful")
 
-    def fetch_hotel_details(self, id):
-        log.debug("Fetching details for hotel id=%s", id)
-        # df = pd.read_csv("hotels.csv")
-        hotel_details = df[df["id"] == id].iloc[0].to_dict()
-        return  hotel_details
-
     def is_hotel_id_available(self, h_id) -> bool:
         log.debug(f'Checking availability for hotel id={h_id}')
-        hotels_list = pd.read_csv("hotels.csv")
-        hotel_ids = hotels_list[hotels_list["availability"] == "yes"]["id"].tolist()
+        hotel_ids = df[df["availability"] == "yes"]["id"].tolist()
         log.debug(f'hotel_ids->{hotel_ids} and id -> {h_id}')
         
         if h_id in hotel_ids:
@@ -83,37 +73,85 @@ class Hotel:
         log.debug("Hotel id=%s is not available", h_id)
         return False
 
+    def make_all_hotels_available(self):
+        log.debug("Making all hotels available")
+        df = pd.read_csv("hotels.csv")
+        df["availability"] = "yes"
+        df.to_csv("hotels.csv", index=False)
+        print("All hotels are now available for booking.")
 
 class ReserveTickets:
     def __init__(self, hotel):
         self.hotel = hotel
-        
+
     def generate_tickets(self):
         pass
 
 class Payment:
-    def cash_payment(self):
-        pass
-    def online_payment(self):
-        pass
+    def __init__(self, amount):
+        self.amount = amount
+    def do_process(self):
+        log.debug("Processing payment of amount %s", self.amount)
+        print(f"Payment of amount {self.amount} processed successfully.")
+        return True    
+        
+    def payment_receipt(self):
+        log.debug("please find the payment receipt for the amount %s", self.amount)
+        return True
 
+class CreditCardPayment(Payment):
+    def __init__(self,amount, number,exp,cvv,holder_name):
+        super().__init__(amount)
+        self.number = number
+        self.exp = exp
+        self.cvv = cvv
+        self.holder_name = holder_name
+
+    def validate_Card(self):
+        card_data = {
+            "number": self.number,
+            "exp": self.exp,
+            "cvv": self.cvv,
+            "holder_name": self.holder_name
+        }
+        if card_data in df_card:
+            log.debug("Credit card validated successfully for holder_name=%s", self.holder_name)
+            print("Credit card validated successfully")
+            return True
+        log.debug("Credit card validation failed for holder_name=%s", self.holder_name)
+        return False
+
+        
 class Welcome:
     def welcome_user(self):
         print("--- Welcome to Agoda Hotel Booking ---\n\n")
 
+df = pd.read_csv("hotels.csv",dtype={"id": str,"availability": str, "24X7": str})
+df_card = pd.read_csv("cards.csv").to_dict(orient='records')  # Convert to list of dictionaries for easier comparison
+print(df_card,'\n')
+
 def main():
     welcome = Welcome()
     welcome.welcome_user()
+
     print(df.to_string(index=False),'\n\n')
-    hotel_booking_id = int(input(" Enter hotel id for booking:"))
+    hotel_booking_id = input(" Enter hotel id for booking:")
     hotel = Hotel(hotel_booking_id,is_availability_24x7=True)
+    hotel.make_all_hotels_available()  # Reset availability for testing purposes
     hotel.view_hotels()
-    if hotel.book_hotel():
-        log.debug("Hotel booking successful for id=%s", hotel_booking_id)
-        ticket = ReserveTickets(hotel)
-        ticket.generate_tickets()
+    creditcard = CreditCardPayment(amount=8889, number=1234, exp="12/26", cvv=111, holder_name="DESH")
+    if creditcard.validate_Card():
+        creditcard.do_process()
+        creditcard.payment_receipt()
+        if hotel.book_hotel():
+            print(f"Hotel booking successful for id={hotel_booking_id}")
+            log.debug("Hotel booking successful for id=%s", hotel_booking_id)
+            ticket = ReserveTickets(hotel)
+            ticket.generate_tickets()
+        else:
+            log.debug("Hotel booking unsuccessful for id=%s", hotel_booking_id)
     else:
-        log.debug("Hotel booking unsuccessful for id=%s", hotel_booking_id)
+        print("Credit card validation failed. Cannot proceed with booking.")
 
 
 if __name__ == "__main__":
